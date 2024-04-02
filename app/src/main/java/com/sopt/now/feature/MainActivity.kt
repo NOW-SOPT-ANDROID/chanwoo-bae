d4836b8518c9fa83d144652845308090451ee5bb
chanu <cw8881@naver.com>
2024-04-03 04:45:44 +0900

    #1 [add] : LoginActivity, SignUpActivity 추가

---
+++ app/src/main/java/com/sopt/now/feature/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.now.feature
+
+import com.sopt.now.R
+import com.sopt.now.core.base.BindingActivity
+import com.sopt.now.databinding.ActivityMainBinding
+
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
---
+++ app/src/main/java/com/sopt/now/feature/auth/SignUpActivity.kt
@@ -0,0 +1,11 @@
+package com.sopt.now.feature.auth
+
+import com.sopt.now.R
+import com.sopt.now.core.base.BindingActivity
+import com.sopt.now.databinding.ActivityMainBinding
+
+class SignUpActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+}
