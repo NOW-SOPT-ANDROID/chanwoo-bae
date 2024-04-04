79cfc81b86853e3f137ce01b450158c8e0c07582
chanu <cw8881@naver.com>
2024-04-05 01:50:04 +0900

    #3 [refact] : 패키징 구조 변경

--- app/src/main/java/com/sopt/now/compose/feature/MainActivity.kt
+++ app/src/main/java/com/sopt/now/compose/feature/MainActivity.kt
@@ -1,15 +1,19 @@
-package com.sopt.now.compose
+package com.sopt.now.compose.feature
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sopt.now.compose.ui.theme.NOWSOPTAndroidTheme
 
 class MainActivity : ComponentActivity() {
@@ -37,10 +41,29 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun FilledTonalButtonExample(onClick: () -> Unit) {
+    FilledTonalButton(onClick = { onClick() }) {
+        Text("Tonal")
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Column(
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxSize() // 전체 화면 크기로 채움
+    ) {
+        // Box 내에 원하는 컨텐츠를 추가할 수 있습니다.
+        FilledTonalButtonExample(onClick = {})
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NOWSOPTAndroidTheme {
-        Greeting("Android")
+        MainScreen()
     }
 }
