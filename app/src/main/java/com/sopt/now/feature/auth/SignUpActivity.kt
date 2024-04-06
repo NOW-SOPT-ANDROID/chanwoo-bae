076f98919d135e2c3bde610e822791ccaedad7b6
chanu <cw8881@naver.com>
2024-04-07 04:11:23 +0900

    #1 [feat] : 회원가입 화면 State로 리팩토링

--- app/src/main/java/com/sopt/now/feature/auth/SignUpActivity.kt
+++ app/src/main/java/com/sopt/now/feature/auth/SignUpActivity.kt
@@ -1,25 +1,31 @@
 package com.sopt.now.feature.auth
 
 import android.content.Intent
+import androidx.activity.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
 import com.sopt.now.core.util.context.snackBar
 import com.sopt.now.core.util.context.toast
+import com.sopt.now.core.view.UiState
 import com.sopt.now.databinding.ActivitySignUpBinding
 import com.sopt.now.feature.model.User
 import com.sopt.now.feature.util.KeyStorage
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_sign_up) {
+    private val viewModel by viewModels<SignUpViewModel>()
 
     override fun initView() {
         initSignUpBtnClickListener()
+        initSignUpStateObserve()
     }
 
     private fun initSignUpBtnClickListener() {
         binding.btnSignUp.setOnClickListener {
-            val userInputData = getUserInputData()
-            val validationMessages = validateUserInputData(userInputData)
-            handleValidationResult(validationMessages, userInputData)
+            viewModel.setUser(getUserInputData())
         }
     }
 
@@ -34,113 +40,29 @@ class SignUpActivity : BindingActivity<ActivitySignUpBinding>(R.layout.activity_
         }
     }
 
-    private fun validateUserInputData(user: User): List<String> {
-        val validationMessages = mutableListOf<String>()
+    private fun initSignUpStateObserve() {
+        viewModel.signUpState.flowWithLifecycle(lifecycle).onEach { state ->
+            when (state) {
+                is UiState.Success -> {
+                    toast("회원가입이 완료되었습니다.")
+                    navigateToLoginActivity(state.data)
+                }
 
-        checkIdLength(user, validationMessages)
+                is UiState.Failure -> {
+                    snackBar(binding.root, state.errorMessage)
+                }
 
-        checkPwdLength(user, validationMessages)
-
-        checkNickNameLength(user, validationMessages)
-
-        checkMbtiLength(user, validationMessages)
-
-        return validationMessages
-    }
-
-    private fun checkIdLength(
-        user: User,
-        validationMessages: MutableList<String>
-    ) {
-        validateLength(user.id, MIN_ID_LENGTH, MAX_ID_LENGTH, R.string.login_id_text)?.let {
-            validationMessages.add(it)
-        }
-    }
-
-    private fun checkPwdLength(
-        user: User,
-        validationMessages: MutableList<String>
-    ) {
-        validateLength(
-            user.password,
-            MIN_PWD_LENGTH,
-            MAX_PWD_LENGTH,
-            R.string.login_password_text
-        )?.let {
-            validationMessages.add(it)
-        }
-    }
-
-    private fun checkMbtiLength(
-        user: User,
-        validationMessages: MutableList<String>
-    ) {
-        if (user.mbti.length != MBTI_LENGTH) {
-            validationMessages.add(getString(R.string.sign_up_mbti_snack_message, MBTI_LENGTH))
-        }
-    }
-
-    private fun checkNickNameLength(
-        user: User,
-        validationMessages: MutableList<String>
-    ) {
-        if (user.nickName.length <= MIN_NICKNAME_LENGTH) {
-            validationMessages.add(
-                getString(
-                    R.string.sign_up_nickname_snack_message,
-                    MIN_NICKNAME_LENGTH
-                )
-            )
-        }
-    }
-
-    private fun validateLength(
-        input: String,
-        minLength: Int,
-        maxLength: Int,
-        fieldNameResId: Int
-    ): String? {
-        return if (input.length !in minLength..maxLength) {
-            getString(
-                R.string.sign_up_snack_message,
-                getString(fieldNameResId),
-                minLength,
-                maxLength
-            )
-        } else {
-            null
-        }
-    }
-
-    private fun handleValidationResult(validationMessages: List<String>, userInputData: User) {
-        if (validationMessages.isEmpty()) {
-            navigateToLoginActivity(userInputData)
-            toast("회원가입이 완료되었습니다.")
-        } else {
-            showSnackBar(validationMessages)
-        }
+                else -> Unit
+            }
+        }.launchIn(lifecycleScope)
     }
 
     private fun navigateToLoginActivity(userInputData: User) {
         Intent().apply {
             putExtra(KeyStorage.USER_INPUT, userInputData)
-            setResult(RESULT_OK, this)
+        }.also {
+            setResult(RESULT_OK, it)
             finish()
         }
-    }
-
-    private fun showSnackBar(messages: List<String>) {
-        if (messages.isNotEmpty()) {
-            snackBar(binding.root, messages.joinToString("\n"))
-        }
-    }
-
-    companion object {
-        const val MIN_ID_LENGTH = 6
-        const val MAX_ID_LENGTH = 10
-        const val MIN_PWD_LENGTH = 8
-        const val MAX_PWD_LENGTH = 12
-        const val MIN_NICKNAME_LENGTH = 1
-        const val MBTI_LENGTH = 4
     }
 }
