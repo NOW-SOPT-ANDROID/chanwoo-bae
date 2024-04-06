df55ca1fe06fac9785869338977e8eb95c09f93f
chanu <cw8881@naver.com>
2024-04-07 05:29:44 +0900

    #1 [feat] : flow 활용하여 2번 눌러 앱 종료 로직 추가

--- app/src/main/java/com/sopt/now/feature/MainActivity.kt
+++ app/src/main/java/com/sopt/now/feature/MainActivity.kt
@@ -2,16 +2,25 @@ package com.sopt.now.feature
 
 import android.content.Context
 import android.content.Intent
+import androidx.activity.OnBackPressedCallback
+import androidx.lifecycle.lifecycleScope
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingActivity
+import com.sopt.now.core.util.context.snackBar
 import com.sopt.now.core.util.intent.getSafeParcelable
 import com.sopt.now.databinding.ActivityMainBinding
 import com.sopt.now.feature.model.User
 import com.sopt.now.feature.util.KeyStorage
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.launch
 
 class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
+    private var backPressedTime = 0L
+    private val backPressedFlow = MutableSharedFlow<Unit>()
+
     override fun initView() {
         initReceiveUserData()
+        initBackDoublePressed()
     }
 
     private fun initReceiveUserData() {
@@ -26,6 +35,37 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         tvMainPwdData.text = receivedUserInput.password
         tvMainNicknameData.text = receivedUserInput.nickName
         tvMainMbtiData.text = receivedUserInput.mbti
+    }
+
+    private fun initBackDoublePressed() {
+        onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
+        observeBackPressedFlow()
+    }
+
+    private val onBackPressedCallback = object : OnBackPressedCallback(true) {
+        override fun handleOnBackPressed() {
+            lifecycleScope.launch {
+                backPressedFlow.emit(Unit)
+            }
+        }
+    }
+
+    private fun observeBackPressedFlow() {
+        lifecycleScope.launch {
+            backPressedFlow.collect {
+                handleBackPressed()
+            }
+        }
+    }
+
+    private fun handleBackPressed() {
+        val currentTime = System.currentTimeMillis()
+        if (currentTime - backPressedTime <= 2000) {
+            finish()
+        } else {
+            backPressedTime = currentTime
+            snackBar(binding.root, getString(R.string.main_back_once_pressed_exit))
+        }
     }
 
     companion object {
