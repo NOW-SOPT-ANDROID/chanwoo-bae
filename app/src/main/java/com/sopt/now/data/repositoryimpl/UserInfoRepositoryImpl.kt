0a9daa05261fca36b02036b3282947c62ed642d2
chanu <cw8881@naver.com>
2024-04-08 07:35:38 +0900

    #6 [add] : UserInfoRepositoryImpl 구현체  생성

---
+++ app/src/main/java/com/sopt/now/data/repositoryimpl/UserInfoRepositoryImpl.kt
@@ -0,0 +1,38 @@
+package com.sopt.now.data.repositoryimpl
+
+import com.sopt.now.data.datasource.local.SharedPreferenceDataSource
+import com.sopt.now.data.dto.local.UserDto
+import com.sopt.now.domain.entity.UserEntity
+import com.sopt.now.domain.repository.UserInfoRepository
+import javax.inject.Inject
+
+class UserInfoRepositoryImpl @Inject constructor(
+    private val sharedPreferenceDataSource: SharedPreferenceDataSource
+) : UserInfoRepository {
+    override fun saveUserInfo(user: UserEntity) {
+        sharedPreferenceDataSource.saveUserInfo(
+            userDto = UserDto(
+                id = user.id,
+                password = user.password,
+                nickName = user.nickName,
+                mbti = user.mbti
+            )
+        )
+    }
+
+    override fun saveCheckLogin(isChecked: Boolean) {
+        sharedPreferenceDataSource.checkLogin = isChecked
+    }
+
+    override fun getUserInfo(): UserEntity {
+        return sharedPreferenceDataSource.getUserInfo().toUserEntity()
+    }
+
+    override fun getCheckLogin(): Boolean {
+        return sharedPreferenceDataSource.checkLogin
+    }
+
+    override fun clear() {
+        sharedPreferenceDataSource.clear()
+    }
+}
