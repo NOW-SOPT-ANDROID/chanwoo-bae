558ba5897ebbecac85d369fda90b86359dc1035d
chanu <cw8881@naver.com>
2024-04-08 07:36:10 +0900

    #6 [feat] : entity dto maping 함수 추가

--- app/src/main/java/com/sopt/now/data/dto/local/UserDto.kt
+++ app/src/main/java/com/sopt/now/data/dto/local/UserDto.kt
@@ -1,8 +1,17 @@
 package com.sopt.now.data.dto.local
 
+import com.sopt.now.domain.entity.UserEntity
+
 data class UserDto(
     val id: String,
     val password: String,
     val nickName: String,
     val mbti: String
-)
+) {
+    fun toUserEntity() = UserEntity(
+        id = id,
+        password = password,
+        nickName = nickName,
+        mbti = mbti
+    )
+}
