749ba257d3b9db49e45c144b4704c6b0c33b0a41
chanu <cw8881@naver.com>
2024-04-08 07:38:04 +0900

    #6 [feat] : prentation영역 User Model entity mapping 함수 추가

--- app/src/main/java/com/sopt/now/feature/model/User.kt
+++ app/src/main/java/com/sopt/now/feature/model/User.kt
@@ -1,6 +1,7 @@
 package com.sopt.now.feature.model
 
 import android.os.Parcelable
+import com.sopt.now.domain.entity.UserEntity
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -9,4 +10,11 @@ data class User(
     val password: String,
     val nickName: String,
     val mbti: String
-) : Parcelable
+) : Parcelable {
+    fun toUserEntity() = UserEntity(
+        id,
+        password,
+        nickName,
+        mbti
+    )
+}
