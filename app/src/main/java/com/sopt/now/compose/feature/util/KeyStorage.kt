46014009f860c4549e0b751e071ae8ac6ee0652a
chanu <cw8881@naver.com>
2024-04-19 07:10:19 +0900

    #11 [chore] : 상수 추가

--- app/src/main/java/com/sopt/now/compose/feature/util/KeyStorage.kt
+++ app/src/main/java/com/sopt/now/compose/feature/util/KeyStorage.kt
@@ -2,4 +2,7 @@ package com.sopt.now.compose.feature.util
 
 object KeyStorage {
     const val USER_INPUT = "user_input_list"
+    const val HOME = "home"
+    const val SEARCH = "search"
+    const val MY_PAGE = "my_page"
 }
