118181125c0ec9bd7e1271054efbfe3176fdcdcc
chanu <cw8881@naver.com>
2024-04-18 20:40:54 +0900

    #9 [feat] : 멀티뷰 viewholder 추가

---
+++ app/src/main/java/com/sopt/now/feature/home/adapter/viewholder/TitleLineViewHolder.kt
@@ -0,0 +1,13 @@
+package com.sopt.now.feature.home.adapter.viewholder
+
+import androidx.recyclerview.widget.RecyclerView
+import com.sopt.now.databinding.ItemTitleLineBinding
+import com.sopt.now.feature.model.HomeSealedItem
+
+class TitleLineViewHolder(private val binding: ItemTitleLineBinding) :
+    RecyclerView.ViewHolder(binding.root) {
+
+    fun onBind(titleData: HomeSealedItem.TitleLine) {
+        binding.tvTitle.text = titleData.title
+    }
+}
