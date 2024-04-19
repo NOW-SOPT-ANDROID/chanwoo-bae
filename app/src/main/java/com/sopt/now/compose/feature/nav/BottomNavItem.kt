24c825b0a789bd709cc9f1c9402421d637075972
chanu <cw8881@naver.com>
2024-04-19 22:19:57 +0900

    #11 [chore] : string 추출

--- app/src/main/java/com/sopt/now/compose/feature/nav/BottomNavItem.kt
+++ app/src/main/java/com/sopt/now/compose/feature/nav/BottomNavItem.kt
@@ -5,16 +5,28 @@ import com.sopt.now.compose.R
 import com.sopt.now.compose.feature.util.KeyStorage
 
 sealed class BottomNavItem(
-    val label: String,
+    val label: Int,
     @DrawableRes val icon: Int,
     val screenRoute: String
 ) {
     data object Home :
-        BottomNavItem("홈", R.drawable.ic_home_black_24, KeyStorage.HOME)
+        BottomNavItem(
+            R.string.nav_home,
+            R.drawable.ic_home_black_24,
+            KeyStorage.HOME
+        )
 
     data object Search :
-        BottomNavItem("검색", R.drawable.ic_search_gray_24, KeyStorage.SEARCH)
+        BottomNavItem(
+            R.string.nav_search,
+            R.drawable.ic_search_gray_24,
+            KeyStorage.SEARCH
+        )
 
     data object MyPage :
-        BottomNavItem("마이", R.drawable.ic_my_page_black_24, KeyStorage.MY_PAGE)
+        BottomNavItem(
+            R.string.nav_my_page,
+            R.drawable.ic_my_page_black_24,
+            KeyStorage.MY_PAGE
+        )
 }
