01e98157909f77beb395a90e6d79ba01d21e6fc2
chanu <cw8881@naver.com>
2024-05-03 03:53:57 +0900

    [feat] : SearchFragment state observe

--- app/src/main/java/com/sopt/now/feature/search/SearchFragment.kt
+++ app/src/main/java/com/sopt/now/feature/search/SearchFragment.kt
@@ -1,12 +1,58 @@
 package com.sopt.now.feature.search
 
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.flowWithLifecycle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.sopt.now.R
 import com.sopt.now.core.base.BindingFragment
+import com.sopt.now.core.util.fragment.snackBar
+import com.sopt.now.core.util.fragment.toast
+import com.sopt.now.core.util.fragment.viewLifeCycle
+import com.sopt.now.core.util.fragment.viewLifeCycleScope
+import com.sopt.now.core.view.UiState
 import com.sopt.now.databinding.FragmentSearchBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    private val viewModel by viewModels<SearchViewModel>()
+
+    private var _reqresAdapter: ReqresAdapter? = null
+    private val reqresAdapter: ReqresAdapter
+        get() = requireNotNull(_reqresAdapter)
+
     override fun initView() {
+        initReqresRecyclerView()
+        initObserveReqresState()
+    }
+
+    private fun initObserveReqresState() {
+        viewModel.getReqresUserList(1)
+        viewModel.reqresUserState.flowWithLifecycle(viewLifeCycle).onEach { state ->
+            when (state) {
+                is UiState.Success -> reqresAdapter.submitList(state.data)
+                is UiState.Failure -> snackBar(binding.root, state.errorMessage)
+                is UiState.Loading -> toast("로딩중")
+            }
+        }.launchIn(viewLifeCycleScope)
+    }
+
+    private fun initReqresRecyclerView() {
+        _reqresAdapter = ReqresAdapter().apply {
+            submitList(emptyList())
+        }
+
+        binding.rvReqres.run {
+            adapter = reqresAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _reqresAdapter = null
     }
 }
