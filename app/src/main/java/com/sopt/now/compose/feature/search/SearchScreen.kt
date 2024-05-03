cb76aafab7efe939723d6505aa74c188324016d3
chanu <cw8881@naver.com>
2024-05-04 00:50:23 +0900

    [feat] : open api 서버 통신

--- app/src/main/java/com/sopt/now/compose/feature/search/SearchScreen.kt
+++ app/src/main/java/com/sopt/now/compose/feature/search/SearchScreen.kt
@@ -1,17 +1,62 @@
 package com.sopt.now.compose.feature.search
 
-import androidx.compose.foundation.layout.Box
+import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.sopt.now.compose.component.row.ReqresDummyRow
+import com.sopt.now.compose.core.view.UiState
 
 @Composable
 fun SearchScreen() {
-    Scaffold { paddingValues ->
-        Box(
-            modifier = Modifier.padding(paddingValues)
+    val viewModel: SearchViewModel = viewModel()
+    val context = LocalContext.current
+
+    LaunchedEffect(Unit) {
+        viewModel.getReqresList(1)
+    }
+
+    val reqresListState =
+        viewModel.getReqresListState.collectAsState(initial = UiState.Loading).value
+
+    val reqres = when (reqresListState) {
+        is UiState.Success -> reqresListState.data
+        is UiState.Failure -> {
+            Toast.makeText(context, reqresListState.errorMessage, Toast.LENGTH_SHORT).show()
+            null
+        }
+
+        UiState.Loading -> {
+            Toast.makeText(context, "로딩중", Toast.LENGTH_SHORT).show()
+            null
+        }
+    }
+
+    Scaffold(
+        modifier = Modifier.padding(horizontal = 16.dp)
+    ) { paddingValues ->
+        LazyColumn(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(16.dp)
         ) {
+            if (reqres == null) {
+                return@LazyColumn
+            }
+            items(reqres) { reqresList ->
+                ReqresDummyRow(data = reqresList)
+            }
         }
     }
 }
---
+++ app/src/main/java/com/sopt/now/compose/feature/search/SearchViewModel.kt
@@ -0,0 +1,36 @@
+package com.sopt.now.compose.feature.search
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.now.compose.core.view.UiState
+import com.sopt.now.compose.data.api.ApiFactory
+import com.sopt.now.compose.feature.model.ReqresEntity
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class SearchViewModel : ViewModel() {
+
+    private val _getReqresListState = MutableStateFlow<UiState<List<ReqresEntity>>>(UiState.Loading)
+    val getReqresListState: SharedFlow<UiState<List<ReqresEntity>>> get() = _getReqresListState.asStateFlow()
+
+    fun getReqresList(page: Int) {
+        viewModelScope.launch {
+            runCatching {
+                ApiFactory.ServicePool.userService.getUserList(page)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    val reqresUserList = response.body()?.toReqresList()
+                    if (reqresUserList != null) {
+                        _getReqresListState.emit(UiState.Success(reqresUserList))
+                    } else {
+                        _getReqresListState.emit(UiState.Failure("null"))
+                    }
+                }
+            }.onFailure { throwable ->
+                _getReqresListState.emit(UiState.Failure(throwable.message.toString()))
+            }
+        }
+    }
+}
