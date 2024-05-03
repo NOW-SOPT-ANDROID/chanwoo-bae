1973a5a0ec54faf191ea958d4b45ffc77939d627
chanu <cw8881@naver.com>
2024-05-03 21:26:25 +0900

    [add] : service 추가

---
+++ app/src/main/java/com/sopt/now/compose/api/service/AuthService.kt
@@ -0,0 +1,32 @@
+package com.sopt.now.compose.api.service
+
+import com.sopt.now.compose.api.ApiKeyStorage.INFO
+import com.sopt.now.compose.api.ApiKeyStorage.JOIN
+import com.sopt.now.compose.api.ApiKeyStorage.LOGIN
+import com.sopt.now.compose.api.ApiKeyStorage.MEMBER
+import com.sopt.now.compose.data.request.RequestLoginDto
+import com.sopt.now.compose.data.request.RequestSignUpDto
+import com.sopt.now.compose.data.response.BaseResponse
+import com.sopt.now.data.dto.remote.response.ResponseMemberInfo
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface AuthService {
+    @POST("$MEMBER/$JOIN")
+    suspend fun signUp(
+        @Body request: RequestSignUpDto
+    ): Response<BaseResponse<Unit>>
+
+    @POST("$MEMBER/$LOGIN")
+    suspend fun login(
+        @Body request: RequestLoginDto
+    ): Response<BaseResponse<Unit>>
+
+    @GET("$MEMBER/$INFO")
+    suspend fun getMemberInfo(
+        @Header("memberId") id: Int
+    ): Response<BaseResponse<ResponseMemberInfo>>
+}
---
+++ app/src/main/java/com/sopt/now/compose/api/service/ReqresService.kt
@@ -0,0 +1,12 @@
+package com.sopt.now.compose.api.service
+
+import com.sopt.now.compose.data.response.ResponseReqresDto
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface ReqresService {
+    @GET("api/users")
+    suspend fun getUserList(
+        @Query("page") page: Int
+    ): ResponseReqresDto
+}
