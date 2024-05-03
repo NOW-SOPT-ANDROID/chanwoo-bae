3a92ab8209b50085c4d683d4b4383094a8d59a78
chanu <cw8881@naver.com>
2024-05-03 20:53:55 +0900

    [add] : LoginRepository 추상체, 구현체 추가

---
+++ app/src/main/java/com/sopt/now/data/repositoryimpl/LoginRepositoryImpl.kt
@@ -0,0 +1,30 @@
+package com.sopt.now.data.repositoryimpl
+
+import com.sopt.now.data.datasource.remote.LoginDataSource
+import com.sopt.now.data.dto.remote.request.RequestLoginDto
+import com.sopt.now.data.dto.remote.request.RequestSignUpDto
+import com.sopt.now.domain.entity.BaseResponseEntity
+import com.sopt.now.domain.entity.UserEntity
+import com.sopt.now.domain.repository.LoginRepository
+import javax.inject.Inject
+
+class LoginRepositoryImpl @Inject constructor(
+    private val loginDataSource: LoginDataSource
+) : LoginRepository {
+    override suspend fun signUp(userEntity: UserEntity): Result<BaseResponseEntity> = runCatching {
+        val requestDto = RequestSignUpDto.toSignUpDto(userEntity)
+        loginDataSource.signUp(requestDto).toBaseResponseEntity()
+    }
+
+    override suspend fun login(id: String, pwd: String): Result<Int?> {
+        return runCatching {
+            loginDataSource.login(RequestLoginDto(id, pwd)).headers()["location"]?.toInt()
+        }
+    }
+
+    override suspend fun getMemberInfo(id: Int): Result<UserEntity> {
+        return runCatching {
+            loginDataSource.getMemberInfo(id).data?.toUserEntity() ?: error("Data is null")
+        }
+    }
+}
---
+++ app/src/main/java/com/sopt/now/domain/repository/LoginRepository.kt
@@ -0,0 +1,10 @@
+package com.sopt.now.domain.repository
+
+import com.sopt.now.domain.entity.BaseResponseEntity
+import com.sopt.now.domain.entity.UserEntity
+
+interface LoginRepository {
+    suspend fun signUp(userEntity: UserEntity): Result<BaseResponseEntity>
+    suspend fun login(id: String, pwd: String): Result<Int?>
+    suspend fun getMemberInfo(id: Int): Result<UserEntity>
+}
