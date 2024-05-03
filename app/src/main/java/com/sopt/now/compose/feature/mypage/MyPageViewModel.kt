99f4192aa63b95741782db2d4a3ed61f91d6239f
chanu <cw8881@naver.com>
2024-05-03 23:37:25 +0900

    [feat] : 마이페이지 id값 전달, 서버통신 구현

---
+++ app/src/main/java/com/sopt/now/compose/feature/mypage/MyPageViewModel.kt
@@ -0,0 +1,39 @@
+package com.sopt.now.compose.feature.mypage
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.now.compose.core.view.UiState
+import com.sopt.now.compose.data.api.ApiFactory
+import com.sopt.now.compose.feature.model.User
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import org.json.JSONObject
+
+class MyPageViewModel : ViewModel() {
+
+    private val _getMemberInfoState = MutableStateFlow<UiState<User>>(UiState.Loading)
+    val getMemberInfoState: SharedFlow<UiState<User>> get() = _getMemberInfoState.asStateFlow()
+
+    fun getMemberInfo(id: Int) {
+        viewModelScope.launch {
+            runCatching {
+                ApiFactory.ServicePool.authService.getMemberInfo(id)
+            }.onSuccess { response ->
+                if (response.isSuccessful) {
+                    val data = response.body()?.data
+                    data?.let { _getMemberInfoState.emit(UiState.Success(it.toMemberInfo())) }
+                } else {
+                    val errorBody = response.errorBody()?.string()
+                    val jsonObject = JSONObject(errorBody.toString())
+                    val errorMessage = jsonObject.getString("message")
+
+                    _getMemberInfoState.emit(UiState.Failure(errorMessage.toString()))
+                }
+            }.onFailure { throwable ->
+                _getMemberInfoState.emit(UiState.Failure(throwable.message.toString()))
+            }
+        }
+    }
+}
