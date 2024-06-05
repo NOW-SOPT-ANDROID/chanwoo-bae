ce0df4d9be9b261818f75489419c08c9bd8bd12b
chanu <cw8881@naver.com>
2024-06-06 08:01:28 +0900

    [mod] : 반복되는 구문 함수화

--- app/src/main/java/com/sopt/now/compose/ui/core/factory/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/now/compose/ui/core/factory/ViewModelFactory.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.ViewModelProvider
 import com.sopt.now.compose.data.api.ApiFactory
 import com.sopt.now.compose.data.repository.AuthRepositoryImpl
 import com.sopt.now.compose.data.repository.ReqresRepositoryImpl
+import com.sopt.now.compose.data_api.auth.AuthRepository
 import com.sopt.now.compose.domain.ReqresUseCase
 import com.sopt.now.compose.feature.auth.SignUpViewModel
 import com.sopt.now.compose.feature.mypage.MyPageViewModel
 import com.sopt.now.compose.feature.search.SearchViewModel
 
 class ViewModelFactory : ViewModelProvider.Factory {
+    private val authRepository by lazy { provideAuthRepository() }
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
@@ -21,18 +23,19 @@ class ViewModelFactory : ViewModelProvider.Factory {
             }
 
             modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
-                val repository =
-                    AuthRepositoryImpl(ApiFactory.ServicePool.authService)
-                SignUpViewModel(repository) as T
+                SignUpViewModel(authRepository) as T
             }
 
             modelClass.isAssignableFrom(MyPageViewModel::class.java) -> {
-                val repository =
-                    AuthRepositoryImpl(ApiFactory.ServicePool.authService)
-                MyPageViewModel(repository) as T
+                MyPageViewModel(authRepository) as T
             }
 
             else -> throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
         }
     }
+
+    private fun provideAuthRepository(): AuthRepository {
+        return AuthRepositoryImpl(ApiFactory.ServicePool.authService)
+    }
+
 }
