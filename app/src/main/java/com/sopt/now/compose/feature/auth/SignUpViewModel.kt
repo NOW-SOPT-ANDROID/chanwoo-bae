df6874d46586bf1c8039cbbf3c24e1f2d804ad93
chanu <cw8881@naver.com>
2024-06-06 07:50:45 +0900

    [feat] : SignUpViewModel 의존성 수동 주입

--- app/src/main/java/com/sopt/now/compose/feature/auth/SignUpViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/feature/auth/SignUpViewModel.kt
@@ -2,38 +2,34 @@ package com.sopt.now.compose.feature.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.now.compose.ui.core.view.UiState
-import com.sopt.now.compose.data.api.ApiFactory
-import com.sopt.now.compose.data.api.dto.request.RequestSignUpDto
+import com.sopt.now.compose.data_api.auth.AuthRepository
+import com.sopt.now.compose.model.ApiError
+import com.sopt.now.compose.model.NetWorkConnectError
 import com.sopt.now.compose.model.User
+import com.sopt.now.compose.ui.core.view.UiState
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import org.json.JSONObject
 
-class SignUpViewModel : ViewModel() {
+class SignUpViewModel(
+    private val authRepository: AuthRepository
+) : ViewModel() {
 
     private val _signUpResponseState = MutableSharedFlow<UiState<User>>()
     val signUpResponseState: SharedFlow<UiState<User>> get() = _signUpResponseState.asSharedFlow()
 
     fun postSignUp(userEntity: User) {
         viewModelScope.launch {
-            runCatching {
-                ApiFactory.ServicePool.authService.signUp(RequestSignUpDto.toSignUpDto(userEntity))
-            }.onSuccess { response ->
-                if (response.isSuccessful) {
+            authRepository.signUp(userEntity)
+                .onSuccess {
                     _signUpResponseState.emit(UiState.Success(userEntity))
-                } else {
-                    val errorBody = response.errorBody()?.string()
-                    val jsonObject = JSONObject(errorBody.toString())
-                    val errorMessage = jsonObject.getString("message")
-
-                    _signUpResponseState.emit(UiState.Failure(errorMessage.toString()))
+                }.onFailure {
+                    when (it) {
+                        is ApiError, is NetWorkConnectError ->
+                            _signUpResponseState.emit(UiState.Failure(it.message.toString()))
+                    }
                 }
-            }.onFailure { throwable ->
-                _signUpResponseState.emit(UiState.Failure(throwable.message.toString()))
-            }
         }
     }
 }
