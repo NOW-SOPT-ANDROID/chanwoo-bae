ed742b91e19d4703daa7d6cea4025002b0dee158
chanu <cw8881@naver.com>
2024-06-06 08:27:10 +0900

    [feat] : 로그인 뷰모델 reposiory 패턴 구현

--- app/src/main/java/com/sopt/now/compose/feature/auth/LoginViewModel.kt
+++ app/src/main/java/com/sopt/now/compose/feature/auth/LoginViewModel.kt
@@ -2,40 +2,30 @@ package com.sopt.now.compose.feature.auth
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.sopt.now.compose.ui.core.view.UiState
-import com.sopt.now.compose.data.api.ApiFactory
 import com.sopt.now.compose.data.api.UserIdProvider
-import com.sopt.now.compose.data.api.dto.request.RequestLoginDto
+import com.sopt.now.compose.data_api.auth.AuthRepository
 import com.sopt.now.compose.feature.util.KeyStorage.ID_DEFAULT
+import com.sopt.now.compose.ui.core.view.UiState
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.launch
-import org.json.JSONObject
 
-class LoginViewModel : ViewModel() {
+class LoginViewModel(
+    private val authRepository: AuthRepository
+) : ViewModel() {
     private val _loginResponseState = MutableSharedFlow<UiState<Int>>()
     val loginResponseState: SharedFlow<UiState<Int>> get() = _loginResponseState.asSharedFlow()
 
     fun postLogin(id: String, pwd: String) {
         viewModelScope.launch {
-            runCatching {
-                ApiFactory.ServicePool.authService.login(RequestLoginDto(id, pwd))
-            }.onSuccess { response ->
-                if (response.isSuccessful) {
-                    val userId = response.headers()["location"]
-                    UserIdProvider.setUserId(userId?.toInt() ?: ID_DEFAULT)
-                    _loginResponseState.emit(UiState.Success(userId?.toInt() ?: ID_DEFAULT))
-                } else {
-                    val errorBody = response.errorBody()?.string()
-                    val jsonObject = JSONObject(errorBody.toString())
-                    val errorMessage = jsonObject.getString("message")
-
-                    _loginResponseState.emit(UiState.Failure(errorMessage.toString()))
+            authRepository.login(id, pwd)
+                .onSuccess { userId ->
+                    UserIdProvider.setUserId(userId ?: ID_DEFAULT)
+                    _loginResponseState.emit(UiState.Success(userId ?: ID_DEFAULT))
+                }.onFailure {
+                    _loginResponseState.emit(UiState.Failure(it.message.toString()))
                 }
-            }.onFailure { throwable ->
-                _loginResponseState.emit(UiState.Failure(throwable.message.toString()))
-            }
         }
     }
 }
