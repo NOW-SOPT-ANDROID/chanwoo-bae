92b4f6d79dc7c13121b13fa81566b8201b62c962
chanu <cw8881@naver.com>
2024-06-06 07:48:53 +0900

    [mod] : 불필요한 response 타입 제거

--- app/src/main/java/com/sopt/now/compose/data/api/service/AuthService.kt
+++ app/src/main/java/com/sopt/now/compose/data/api/service/AuthService.kt
@@ -5,7 +5,7 @@ import com.sopt.now.compose.data.api.service.ApiKeyStorage.JOIN
 import com.sopt.now.compose.data.api.service.ApiKeyStorage.LOGIN
 import com.sopt.now.compose.data.api.service.ApiKeyStorage.MEMBER
 import com.sopt.now.compose.data.api.dto.request.RequestLoginDto
-import com.sopt.now.compose.data.api.dto.request.RequestSignUpDto
+import com.sopt.now.compose.data.api.dto.request.SignUpRequest
 import com.sopt.now.compose.data.api.dto.response.BaseResponse
 import com.sopt.now.compose.data.api.dto.response.MemberInfoResponse
 import retrofit2.Response
@@ -16,8 +16,8 @@ import retrofit2.http.POST
 interface AuthService {
     @POST("$MEMBER/$JOIN")
     suspend fun signUp(
-        @Body request: RequestSignUpDto
-    ): Response<BaseResponse<Unit>>
+        @Body request: SignUpRequest
+    ): BaseResponse<Unit>
 
     @POST("$MEMBER/$LOGIN")
     suspend fun login(
@@ -25,5 +25,5 @@ interface AuthService {
     ): Response<BaseResponse<Unit>>
 
     @GET("$MEMBER/$INFO")
-    suspend fun getMemberInfo(): Response<BaseResponse<MemberInfoResponse>>
+    suspend fun getMemberInfo(): BaseResponse<MemberInfoResponse>
 }
