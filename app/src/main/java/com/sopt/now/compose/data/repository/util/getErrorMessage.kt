e85961f09801296ed49ea03e4daeec6c6e663df9
chanu <cw8881@naver.com>
2024-06-06 07:49:25 +0900

    [feat] : getErrorMessage 확장 함수 생성

---
+++ app/src/main/java/com/sopt/now/compose/data/repository/util/getErrorMessage.kt
@@ -0,0 +1,31 @@
+package com.sopt.now.compose.data.repository.util
+
+import com.sopt.now.compose.data.api.dto.response.BaseResponse
+import com.sopt.now.compose.model.ApiError
+import com.sopt.now.compose.model.NetWorkConnectError
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import retrofit2.HttpException
+import java.io.IOException
+
+fun Throwable.getErrorMessage(): String {
+    return when (this) {
+        is HttpException -> {
+            val errorBody = response()?.errorBody()?.string() ?: return "Unknown error"
+            val errorResponse = Json.decodeFromString<BaseResponse<Unit>>(errorBody)
+            errorResponse.message
+        }
+
+        else -> "An unknown error occurred."
+    }
+}
+
+fun <T> Throwable.handleThrowable(): Result<T> {
+    return Result.failure(
+        when (this) {
+            is HttpException -> ApiError(this.getErrorMessage())
+            is IOException -> NetWorkConnectError("인터넷에 연결 해 주세요")
+            else -> this
+        }
+    )
+}
