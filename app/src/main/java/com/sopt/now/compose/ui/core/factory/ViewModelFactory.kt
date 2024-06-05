8fb843db8a9b3d1d70d5885d8cd8f3f05a71d2a2
chanu <cw8881@naver.com>
2024-06-06 07:50:12 +0900

    [feat] : ViewModelFactory AuthRepsoitory 추가

--- app/src/main/java/com/sopt/now/compose/ui/core/factory/ViewModelFactory.kt
+++ app/src/main/java/com/sopt/now/compose/ui/core/factory/ViewModelFactory.kt
@@ -1,14 +1,15 @@
-package com.sopt.now.compose.ui.core.viewmodel
+package com.sopt.now.compose.ui.core.factory
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.sopt.now.compose.data.api.ApiFactory
+import com.sopt.now.compose.data.repository.AuthRepositoryImpl
 import com.sopt.now.compose.data.repository.ReqresRepositoryImpl
 import com.sopt.now.compose.domain.ReqresUseCase
+import com.sopt.now.compose.feature.auth.SignUpViewModel
 import com.sopt.now.compose.feature.search.SearchViewModel
 
-class ViewModelFactory :
-    ViewModelProvider.Factory {
+class ViewModelFactory : ViewModelProvider.Factory {
 
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
         return when {
@@ -16,6 +17,12 @@ class ViewModelFactory :
                 val repository =
                     ReqresUseCase(ReqresRepositoryImpl(ApiFactory.ServicePool.userService))
                 SearchViewModel(repository) as T
+            }
+
+            modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
+                val repository =
+                    AuthRepositoryImpl(ApiFactory.ServicePool.authService)
+                SignUpViewModel(repository) as T
             }
 
             /*  modelClass.isAssignableFrom(SignUpViewModel::class.java) -> {
